1295ec19159696b558830789fbf4eb9491811cb4
= <=>
2022-02-23 14:40:17 +0530

    First commit

---
+++ precache-manifest.9ae4630614688b1b4cc198a14801885a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21871d74de8ae1788ef4164f6401d0e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2cc80d85b706c513042",
+    "url": "/static/css/main.aaa26502.chunk.css"
+  },
+  {
+    "revision": "ca77627b6a30990c1a89",
+    "url": "/static/js/2.3c152af1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3c152af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2cc80d85b706c513042",
+    "url": "/static/js/main.8ee0159e.chunk.js"
+  },
+  {
+    "revision": "f13d5fb20258b992088c",
+    "url": "/static/js/runtime-main.a6b2f021.js"
+  }
+]);
